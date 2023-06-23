e3a3a724098f9f6681f10d1bb17cc63840ee3ee1
gorag <gorbunov.alexey@gmail.com>
2023-06-23 13:28:34 +0300

    fix2

--- main.py
+++ main.py
@@ -5,15 +5,11 @@ import requests
 from dotenv import load_dotenv
 
 
-def shorten_link(token, long_url):
+def shorten_link(headers, long_url):
     url = 'https://api-ssl.bitly.com/v4/shorten'
 
     json_long_url = {
         'long_url': long_url
-    }
-
-    headers = {
-        'Authorization': f'Bearer {token}',
     }
 
     response = requests.post(
@@ -26,13 +22,10 @@ def shorten_link(token, long_url):
     return response.json()['link']
 
 
-def count_clicks(token, link):
+def count_clicks(headers, link):
     parsed_url = urlparse(link)
-    api_url = f'https://api-ssl.bitly.com/v4/bitlinks/bit.ly{parsed_url.path}/clicks/summary'
-    
-    headers = {
-        'Authorization': f'Bearer {token}',
-    }
+    api_url = f'https://api-ssl.bitly.com/v4/bitlinks\
+        /{parsed_url.netloc}{parsed_url.path}/clicks/summary'
 
     response = requests.get(
         url=api_url,
@@ -44,39 +37,40 @@ def count_clicks(token, link):
     return response.json()['total_clicks']
 
 
-def is_bitlink(token, url):
+def is_bitlink(headers, url):
     parsed_url = urlparse(url)
-    api_url = f'https://api-ssl.bitly.com/v4/bitlinks/bit.ly{parsed_url.path}'
-
-    headers = {
-        'Authorization': f'Bearer {token}',
-    }
+    api_url = f'https://api-ssl.bitly.com/v4/bitlinks\
+        /{parsed_url.netloc}{parsed_url.path}'
 
     response = requests.get(
         url=api_url,
         headers=headers,
         timeout=1
     )
-    print(response.json())
     return response.ok
 
 
 if __name__ == '__main__':
     load_dotenv()
-    token = os.getenv('ACCESS_TOKEN')
+    
+    token = os.getenv('BITLY_TOKEN')
+
+    headers = {
+        'Authorization': f'Bearer {token}',
+    }
 
     user_input = input('Введите ссылку: ')
     
-    if is_bitlink(token=token, url=user_input):
+    if is_bitlink(headers=headers, url=user_input):
         try:
-            clicks_count = count_clicks(token=token, link=user_input)
+            clicks_count = count_clicks(headers=headers, link=user_input)
         except requests.exceptions.HTTPError:
             print('Введите правильный битлинк')
         else:
             print(f'По вашей ссылке прошли: {clicks_count} раз(а)')
     else:
         try:
-            bitlink = shorten_link(token=token, long_url=user_input)
+            bitlink = shorten_link(headers=headers, long_url=user_input)
         except requests.exceptions.HTTPError:
             print('Введите правильную ссылку')
         else:
