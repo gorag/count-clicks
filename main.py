6b82a9c40f33b03f209fb9f82ec1e6857db67a5b
Alexey <gorbunov.alexey@gmail.com>
2023-07-14 22:35:21 +0300

    Host the project on GitHub

--- main.py
+++ main.py
@@ -3,75 +3,66 @@ from urllib.parse import urlparse
 
 import requests
 from dotenv import load_dotenv
+import argparse
 
 
-def shorten_link(headers, long_url):
-    url = 'https://api-ssl.bitly.com/v4/shorten'
+def shorten_link(bitly_headers, long_url):
+    url = "https://api-ssl.bitly.com/v4/shorten"
 
-    json_long_url = {
-        'long_url': long_url
-    }
+    json_long_url = {"long_url": long_url}
 
     response = requests.post(
-        url=url,
-        headers=headers,
-        json=json_long_url,
-        timeout=1)
-        
+        url=url, headers=bitly_headers, json=json_long_url, timeout=1
+    )
+
     response.raise_for_status()
-    return response.json()['link']
+    return response.json()["link"]
 
 
-def count_clicks(headers, link):
+def count_clicks(bitly_headers, link):
     parsed_url = urlparse(link)
-    api_url = f'https://api-ssl.bitly.com/v4/bitlinks\
-        /{parsed_url.netloc}{parsed_url.path}/clicks/summary'
+    api_url = f"https://api-ssl.bitly.com/v4/bitlinks/{parsed_url.netloc}{parsed_url.path}/clicks/summary"
 
-    response = requests.get(
-        url=api_url,
-        headers=headers,
-        timeout=1
-    )
+    response = requests.get(url=api_url, headers=bitly_headers, timeout=1)
 
     response.raise_for_status()
-    return response.json()['total_clicks']
+    return response.json()["total_clicks"]
 
 
-def is_bitlink(headers, url):
+def is_bitlink(bitly_headers, url):
     parsed_url = urlparse(url)
-    api_url = f'https://api-ssl.bitly.com/v4/bitlinks\
-        /{parsed_url.netloc}{parsed_url.path}'
-
-    response = requests.get(
-        url=api_url,
-        headers=headers,
-        timeout=1
+    api_url = (
+        f"https://api-ssl.bitly.com/v4/bitlinks/{parsed_url.netloc}{parsed_url.path}"
     )
+
+    response = requests.get(url=api_url, headers=bitly_headers, timeout=1)
     return response.ok
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     load_dotenv()
-    
-    token = os.getenv('BITLY_TOKEN')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("url", help="URL для сокращения или битлинк")
+    args = parser.parse_args()
+
+    token = os.getenv("BITLY_TOKEN")
 
     headers = {
-        'Authorization': f'Bearer {token}',
+        "Authorization": f"Bearer {token}",
     }
 
-    user_input = input('Введите ссылку: ')
-    
-    if is_bitlink(headers=headers, url=user_input):
+    if is_bitlink(bitly_headers=headers, url=args.url):
         try:
-            clicks_count = count_clicks(headers=headers, link=user_input)
+            clicks_count = count_clicks(bitly_headers=headers, link=args.url)
         except requests.exceptions.HTTPError:
-            print('Введите правильный битлинк')
+            print("Введите правильный битлинк")
         else:
-            print(f'По вашей ссылке прошли: {clicks_count} раз(а)')
+            print(f"По вашей ссылке прошли: {clicks_count} раз(а)")
     else:
         try:
-            bitlink = shorten_link(headers=headers, long_url=user_input)
+            bitlink = shorten_link(bitly_headers=headers, long_url=args.url)
         except requests.exceptions.HTTPError:
-            print('Введите правильную ссылку')
+            print("Введите правильную ссылку")
         else:
-            print(f'Ваш битлинк: {bitlink}')
+            print(f"Ваш битлинк: {bitlink}")
